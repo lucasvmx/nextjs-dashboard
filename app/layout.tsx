fbf8703ae7693df1a55c1acd029179055a324767
Lucas Vieira <lucas.engen.cc@gmail.com>
2025-01-04 12:43:40 -0300

    chore: adjusts colors and run lint
    Signed-off-by: Lucas Vieira <lucas.engen.cc@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-            <body className={`${inter.className} antialiased`}>{children}</body>
+            <body className={`${inter.className} antialiased bg-purple-300`}>{children}</body>
         </html>
     );
 }
